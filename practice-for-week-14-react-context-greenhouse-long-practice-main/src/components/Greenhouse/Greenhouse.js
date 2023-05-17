d536ed9c7b3bd894df193adcf70dad705b5d1717
herilynn <4henrylin17@gmail.com>
2023-05-17 14:28:37 -0400

    Finish phase1

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
@@ -4,13 +4,23 @@ import './Greenhouse.css';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
+import {useTheme} from '../../context/ThemeContext';
 
 function Greenhouse() {
+  // const[count, setCount] = useState()
+  const{themeName} = useTheme()
+
+let src = null
+if (themeName === 'day') {
+  src = dayImage
+} else if (themeName === 'night') {
+  src = nightImage
+}
 
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={src}
             alt='greenhouse' 
       />
       <LightSwitch />
